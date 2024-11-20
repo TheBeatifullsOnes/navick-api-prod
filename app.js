ed2163f5359446949e1c03cb2bad7c59ef5d3890
luiis29 <51802134+luiis29@users.noreply.github.com>
2024-11-19 20:18:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(500).json({ error: "err" });
+  res.status(500).json({ error: "err error" });
 });
 
 export default app;
